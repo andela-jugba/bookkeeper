811521b3bb2bf79f11921616903cf31e8cdb7b0a
Joshua Ugba <justjosh001@gmail.com>
2016-08-05 09:17:38 +0100

    change config

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from app import create_app
+from flask_migrate import upgrade
 application = create_app('production')
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8080, application)
-    httpd.serve_forever()
+    # from wsgiref.simple_server import make_server
+    # httpd = make_server('localhost', 8080, application)
+    # httpd.serve_forever()
+    application.run(port=8080)
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
+from flask_migrate import upgrade
 from app import create_app
 application = create_app('production')
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8080, application)
-    httpd.serve_forever()
+    # from wsgiref.simple_server import make_server
+    # httpd = make_server('localhost', 8080, application)
+    # httpd.serve_forever()
+    application.run(port=8080)
