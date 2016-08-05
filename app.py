5c3363868b0f8815865b2d8f74fe8d4d5d6a1a0d
Joshua Ugba <justjosh001@gmail.com>
2016-08-05 05:23:05 +0100

    change port

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ application = create_app('production')
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
+    httpd = make_server('localhost', 8080, application)
     httpd.serve_forever()
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 import os
 from app import create_app
 application = create_app('production')
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
+    httpd = make_server('localhost', 8080, application)
     httpd.serve_forever()
